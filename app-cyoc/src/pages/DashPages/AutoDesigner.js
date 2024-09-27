727fcec92110860a2e809e0ad2809c2650bdc582
SantinoBrr <santinodarioscotton@gmail.com>
2024-09-27 16:21:55 -0300

    Actualizacion de AutoDesigner.js para mejor funcionamiento

--- app-cyoc/src/pages/DashPages/AutoDesigner.js
+++ app-cyoc/src/pages/DashPages/AutoDesigner.js
@@ -13,30 +13,29 @@ function AutoDesigner() {
         SELECT CAR PARTS
       </header>
       <div className="container">
-        <div className="option" id="chasis" onClick={() => handlePartSelect('chasis')}>
+        <div className="option" id="chasis">
           <div className="label">CHASSIS</div>
-          <button className="choose">CHOOSE</button>
+          <button className="choose" onClick={() => handlePartSelect('chasis')}>CHOOSE</button>
           <div className="description">The car chassis is the base structure supporting all mechanical components and the bodywork.</div>
         </div>
-        <div className="option" id="llanta" onClick={() => handlePartSelect('wheels')}>
+        <div className="option" id="llanta">
           <div className="label">WHEEL</div>
-          <button className="choose">CHOOSE</button>
+          <button className="choose" onClick={() => handlePartSelect('wheels')}>CHOOSE</button>
           <div className="description">The wheels are essential for vehicle movement and support the entire weight.</div>
         </div>
-        <div className="option" id="motor" onClick={() => handlePartSelect('engine')}>
+        <div className="option" id="motor">
           <div className="label">ENGINE</div>
-          <button className="choose">CHOOSE</button>
+          <button className="choose" onClick={() => handlePartSelect('engine')}>CHOOSE</button>
           <div className="description">The engine is the heart of the vehicle, converting fuel into motion.</div>
         </div>
-        <div className="option" id="color" onClick={() => handlePartSelect('color')}>
+        <div className="option" id="color">
           <div className="label">COLOR</div>
-          <button className="choose">CHOOSE</button>
+          <button className="choose" onClick={() => handlePartSelect('color')}>CHOOSE</button>
           <div className="description">Color is the aesthetic choice for your car's exterior.</div>
         </div>
       </div>
       <div>
-      {selectedPart && <p className='p-selectpart'>Selected part: {selectedPart}</p>}
-
+        {selectedPart && <p className='p-selectpart'>Selected part: {selectedPart}</p>}
       </div>
     </div>
   );

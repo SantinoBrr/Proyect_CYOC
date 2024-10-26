e873c9913829ede0335898d6516349ad5b358d52
Tobias <tobicarosso@gmail.com>
2024-10-26 19:30:21 -0300

    cambio de lugar los sonidos, y cambio de backgroundcolor del selectedparts.

--- app-cyoc/src/pages/DashPages/AutoDesigner.js
+++ app-cyoc/src/pages/DashPages/AutoDesigner.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { HuePicker } from 'react-color';
-import { useNavigate } from 'react-router-dom'; // Para la navegación
+import { useNavigate } from 'react-router-dom';
 import '../../assets/styles/AutoDesigner.css';
 
-// Importa las imágenes de las ruedas
+
 import rueda1 from '../../assets/images/ruedas_modelos/rueda1.jpg';
 import rueda2 from '../../assets/images/ruedas_modelos/rueda2.jpg';
 import rueda3 from '../../assets/images/ruedas_modelos/rueda3.jpg';
@@ -13,73 +13,71 @@ import rueda6 from '../../assets/images/ruedas_modelos/rueda6.jpg';
 import rueda7 from '../../assets/images/ruedas_modelos/rueda7.jpg';
 import rueda8 from '../../assets/images/ruedas_modelos/rueda8.jpg';
 
-// Importa las imágenes de los chasis
+
 import dodge from '../../assets/images/chasis_modelos/dodge.jpg';
 
-// Array de motores con nombre y sonido
+
 const motores = [
-  { nombre: '5.2 FSI V10', sonido: '/sounds/audi.mp3' },
-  { nombre: 'M TwinPower Turbo V8', sonido: '/sounds/bmw.mp3' },
-  { nombre: 'HEMI 5.7 SUPERCARGADO', sonido: '/sounds/dodge.mp3' },
-  { nombre: 'Ferrari F120A', sonido: '/sounds/ferrari.mp3' },
-  { nombre: 'Wankel 13B', sonido: '/sounds/mazda.mp3' },
-  { nombre: 'M178 V8 Biturbo 4.0', sonido: '/sounds/mercedes.mp3' },
-  { nombre: 'Coyote 5.0L Ti-VCT V8', sonido: '/sounds/mustang.mp3' },
-  { nombre: 'RB26DETT 2.6 Turbo', sonido: '/sounds/nissan.mp3' },
+  { nombre: '5.2 FSI V10', sonido: '../../assets/audio/motores_sonidos/audi.mp3' },
+  { nombre: 'M TwinPower Turbo V8', sonido: '../../assets/audio/motores_sonidos/bmw.mp3' },
+  { nombre: 'HEMI 5.7 SUPERCARGADO', sonido: '../../assets/audio/motores_sonidos/dodge.mp3' },
+  { nombre: 'Ferrari F120A', sonido: '../../assets/audio/motores_sonidos/ferrari.mp3' },
+  { nombre: 'Wankel 13B', sonido: '../../assets/audio/motores_sonidos/mazda.mp3' },
+  { nombre: 'M178 V8 Biturbo 4.0', sonido: '../../assets/audio/motores_sonidos/mercedes.mp3' },
+  { nombre: 'Coyote 5.0L Ti-VCT V8', sonido: '../../assets/audio/motores_sonidos/mustang.mp3' },
+  { nombre: 'RB26DETT 2.6 Turbo', sonido: '../../assets/audio/motores_sonidos/nissan.mp3' },
 ];
 
 function AutoDesigner() {
   const [selectedPart, setSelectedPart] = useState(null);
   const [color, setColor] = useState('#ffffff');
   const [showColorPicker, setShowColorPicker] = useState(false);
-  const [selectedWheel, setSelectedWheel] = useState(null); // Estado para rueda seleccionada
-  const [selectedChassis, setSelectedChassis] = useState(null); // Estado para chasis seleccionado
-  const [selectedEngine, setSelectedEngine] = useState(null); // Estado para motor seleccionado
-  const navigate = useNavigate(); // Hook para navegación
+  const [selectedWheel, setSelectedWheel] = useState(null); 
+  const [selectedChassis, setSelectedChassis] = useState(null); 
+  const [selectedEngine, setSelectedEngine] = useState(null);
+  const navigate = useNavigate(); 
 
-  // Lista de imágenes de las ruedas
+
   const wheelImages = [rueda1, rueda2, rueda3, rueda4, rueda5, rueda6, rueda7, rueda8];
 
-  // Lista de imágenes de chasis
+
   const chassisImages = [dodge, dodge, dodge, dodge, dodge, dodge, dodge, dodge];
 
-  // Función para seleccionar una parte del auto
+
   const handlePartSelect = (part) => {
     setSelectedPart(part);
   };
 
-  // Función para cambiar el color desde la paleta
   const handleColorChange = (color) => {
-    setColor(color.hex); // Actualiza el color con el círculo de la paleta
+    setColor(color.hex);
   };
 
-  // Función para cambiar el color desde el input
   const handleInputChange = (e) => {
-    setColor(e.target.value); // Actualiza el color con el input manual
+    setColor(e.target.value);
   };
 
-  // Función para seleccionar una rueda
+
   const handleWheelSelect = (index) => {
-    setSelectedWheel(index); // Actualiza la rueda seleccionada
+    setSelectedWheel(index);
   };
 
-  // Función para seleccionar un chasis
+  
   const handleChassisSelect = (index) => {
-    setSelectedChassis(index); // Actualiza el chasis seleccionado
+    setSelectedChassis(index); 
   };
 
-  // Función para seleccionar un motor
+  
   const handleEngineSelect = (index) => {
-    setSelectedEngine(index); // Actualiza el motor seleccionado
+    setSelectedEngine(index); 
   };
 
-  // Función para verificar si todas las partes están seleccionadas
+ 
   const allPartsSelected = selectedChassis !== null && selectedWheel !== null && selectedEngine !== null && color !== '';
 
-  // Función para manejar la navegación
+ 
   const handleContinue = () => {
     if (allPartsSelected) {
-      // Navegar a otra página y pasar los datos seleccionados como estado
+    
       navigate('/create-car/final-page', {
         state: {
           selectedChassis,
@@ -97,7 +95,7 @@ function AutoDesigner() {
         SELECT CAR PARTS
       </header>
       <div className="container">
-        {/* CHASIS */}
+
         <div className="option" id="chasis" onMouseEnter={() => handlePartSelect('chasis')}>
           <div className="label">CHASSIS</div>
           <div className="grid-container">
@@ -113,7 +111,7 @@ function AutoDesigner() {
           </div>
         </div>
 
-        {/* WHEELS */}
+
         <div className="option" id="llanta" onMouseEnter={() => handlePartSelect('wheels')}>
           <div className="label">WHEEL</div>
           <div className="grid-container">
@@ -129,7 +127,6 @@ function AutoDesigner() {
           </div>
         </div>
 
-        {/* MOTOR */}
         <div className="option" id="motor" onMouseEnter={() => handlePartSelect('engine')}>
           <div className="label">ENGINE</div>
           <div className="grid-container">
@@ -139,18 +136,16 @@ function AutoDesigner() {
                 className={`grid-box ${selectedEngine === index ? 'selected' : ''}`}
                 onClick={() => handleEngineSelect(index)}
               >
-                {motor.nombre} {/* Mostrar el nombre completo del motor */}
+                {motor.nombre}
               </button>
             ))}
           </div>
         </div>
-
-        {/* COLOR */}
         <div 
           className="option" 
           id="color" 
           onMouseEnter={() => setShowColorPicker(true)} 
-          onMouseLeave={() => setShowColorPicker(false)} // Ocultar al sacar el mouse
+          onMouseLeave={() => setShowColorPicker(false)}
         >
           <div className="label">COLOR</div>
           {showColorPicker && (
@@ -162,7 +157,7 @@ function AutoDesigner() {
                 onChange={handleInputChange}
                 placeholder="#000000"
                 className="color-input"
-                style={{ backgroundColor: color }} // Cambia el fondo al color seleccionado
+                style={{ backgroundColor: color }}
               />
             </div>
           )}
@@ -177,7 +172,6 @@ function AutoDesigner() {
         )}
       </div>
 
-      {/* Mostrar el botón CONTINUE cuando todas las partes estén seleccionadas */}
       {allPartsSelected && (
         <div style={{ textAlign: 'center', marginTop: '20px' }}>
           <button onClick={handleContinue} className="continue-button">CONTINUE</button>-
--- app-cyoc/src/pages/DashPages/FinalStep.js
+++ app-cyoc/src/pages/DashPages/FinalStep.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import '../../assets/styles/finalstep.css'; 
-
+import '../../assets/styles/finalstep.css';
 
 function FinalStep() {
   const location = useLocation();
-  const selectedParts = location.state || {}; // Obtén los datos del estado o un objeto vacío
+  const selectedParts = location.state || {};
 
   const [vehicleName, setVehicleName] = useState('');
   const [description, setDescription] = useState('');
@@ -38,28 +37,41 @@ function FinalStep() {
       <div className="form-group">
         <label htmlFor="description">Description:</label>
         <textarea
-          id="description"
-          value={description}
-          onChange={(e) => {
-            setDescription(e.target.value);
-            handleInputChange();
-          }}
+        id="description"
+        className="description-textarea"
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+          handleInputChange();
+        }}
         />
       </div>
 
-      {/* Mostrar las partes seleccionadas en un grid 2x2 */}
-      <div className="grid-container">
-        <div className="grid-item">
+
+      <div className="final-grid-container">
+        <div className="final-grid-item">
           Selected Engine: {selectedParts.selectedEngine?.nombre || 'No engine selected'}
         </div>
-        <div className="grid-item">
+        <div className="final-grid-item">
           Selected Wheel: {selectedParts.selectedWheel + 1 || 'No wheel selected'}
         </div>
-        <div className="grid-item">
+        <div className="final-grid-item">
           Selected Chassis: Chassis {selectedParts.selectedChassis + 1 || 'No chassis selected'}
         </div>
-        <div className="grid-item">
-          Selected Color: <span style={{ backgroundColor: selectedParts.selectedColor }}>{selectedParts.selectedColor}</span>
+        <div className="final-grid-item">
+          Selected Color: 
+          <span className="color-code">{selectedParts.selectedColor}</span>
+          <div
+            className="color-square"
+            style={{
+              backgroundColor: selectedParts.selectedColor,
+              width: '20px',
+              height: '20px',
+              display: 'inline-block',
+              marginLeft: '10px',
+              border: '1px solid #ccc',
+            }}
+          ></div>
         </div>
       </div>
 
@@ -73,7 +85,6 @@ function FinalStep() {
         </button>
       </div>
 
-      {/* Reproductor de pista de sonido del motor */}
       {selectedParts.selectedEngine?.sonido && (
         <div className="audio-player">
           <audio controls>
